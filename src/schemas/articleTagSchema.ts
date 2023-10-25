09285fbfbfb4847dd19d817996826070bbd736e9
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-10-25 18:31:12 -0300

    add messages in article tag schema

--- src/schemas/articleTagSchema.ts
+++ src/schemas/articleTagSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const articleTagSchema = z.object({
-	id: z.string().uuid(),
-	articleId: z.string().uuid(),
-	tagId: z.string().uuid(),
+	id: z.string().uuid({ message: "Deve ser um ID único universal" }),
+	articleId: z.string().uuid({ message: "Deve ser um ID único universal" }),
+	tagId: z.string().uuid({ message: "Deve ser um ID único universal" }),
 });
