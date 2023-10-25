812d4763e9439ca07c85f5671728ebd365ec799c
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-10-25 18:29:47 -0300

    add messages in user schema

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,8 +1,13 @@
 import { z } from "zod";
 
 export const userSchema = z.object({
-	id: z.string().uuid(),
-	name: z.string().min(3).max(100),
-	email: z.string().email().min(5),
-	password: z.string().min(8),
+	id: z.string().uuid({ message: "Deve ser um ID único universal" }),
+	name: z
+		.string()
+		.min(1, { message: "Deve ter no mínimo 1 caractere" })
+		.max(100, { message: "Deve ter no máximo 100 caracteres" }),
+	email: z.string().email({ message: "Deve ser um e-mail válido" }),
+	password: z
+		.string()
+		.min(8, { message: "A senha deve ter no mínimo 8 caracteres" }),
 });
