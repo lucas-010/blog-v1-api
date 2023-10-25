e1a965cfe43acb4dafd0e9093037cc0e30f044f4
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-10-25 18:30:37 -0300

    add messages in comment schema

--- src/schemas/commentSchema.ts
+++ src/schemas/commentSchema.ts
@@ -1,9 +1,10 @@
 import { z } from "zod";
 
 export const commentSchema = z.object({
-	id: z.string().uuid(),
-	title: z.string().min(2),
-	body: z.string(),
+	id: z.string().uuid({ message: "Deve ser um ID único universal" }),
+	title: z.string().min(2, { message: "Deve ter no mínimo 2 caracteres" }),
+	text: z.string().min(1, { message: "Deve ter no mínimo 1 caractere" }),
 	published: z.date(),
-	userId: z.string().uuid(),
+	articleId: z.string().uuid({ message: "Deve ser um ID único universal" }),
+	userId: z.string().uuid({ message: "Deve ser um ID único universal" }),
 });
