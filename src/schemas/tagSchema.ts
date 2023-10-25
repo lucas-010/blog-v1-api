a11f51b3ef98e16659cb845e06ae9b9225fbd490
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-10-25 18:30:19 -0300

    add messages in tag schema

--- src/schemas/tagSchema.ts
+++ src/schemas/tagSchema.ts
@@ -1,6 +1,9 @@
 import { z } from "zod";
 
 export const tagSchema = z.object({
-	id: z.string().uuid(),
-	name: z.string().min(2).max(15),
+	id: z.string().uuid({ message: "Deve ser um ID único universal" }),
+	name: z
+		.string()
+		.min(2, "Deve ter no mínimo 2 caracteres")
+		.max(20, "Deve ter no máximo 20 caracteres"),
 });
