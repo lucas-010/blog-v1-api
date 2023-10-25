3c7dcacf0381ffe49fba97bebec5db446069abb1
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-10-25 18:30:56 -0300

    add messages in article schema

--- src/schemas/articleSchema.ts
+++ src/schemas/articleSchema.ts
@@ -1,8 +1,9 @@
 import { z } from "zod";
 
 export const articleSchema = z.object({
-	id: z.string().uuid(),
-	text: z.string(),
-	articleId: z.string().uuid(),
-	userId: z.string().uuid(),
+	id: z.string().uuid({ message: "Deve ser um ID único universal" }),
+	title: z.string().min(2, { message: "Deve ter no mínimo 2 caracteres" }),
+	body: z.string().min(1, { message: "Deve ter no mínimo 1 caractere" }),
+	published: z.date(),
+	userId: z.string().uuid({ message: "Deve ser um ID único universal" }),
 });
