f72ee403a0cd8b02f71868e55c8c46301286897f
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-10-29 00:13:22 -0300

    comment schema removed title

--- src/schemas/commentSchema.ts
+++ src/schemas/commentSchema.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 export const commentSchema = z.object({
 	id: z.string().uuid({ message: "Deve ser um ID único universal" }),
-	title: z.string().min(2, { message: "Deve ter no mínimo 2 caracteres" }),
 	text: z.string().min(1, { message: "Deve ter no mínimo 1 caractere" }),
 	published: z.date(),
 	articleId: z.string().uuid({ message: "Deve ser um ID único universal" }),
