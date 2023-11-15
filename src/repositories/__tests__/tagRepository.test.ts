c63ad6d45d3a1d71c1e27112ea56ad2f9a739ef4
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-11-15 19:46:13 -0300

    tag repository tests select all test with page and limit

--- src/repositories/__tests__/tagRepository.test.ts
+++ src/repositories/__tests__/tagRepository.test.ts
@@ -6,34 +6,34 @@ describe("Tag - Repository", () => {
 	const tagRepository = new TagRepository();
 
 	it("should return an id according to the class when inserting tag", async () => {
-		const tag = new Tag("Test Tag");
+		const tag = new Tag({ name: "Test Tag" });
 		const result = await tagRepository.insert({ name: tag.name, id: tag.id });
 		expect(result).toEqual(tag.id);
 		await tagRepository.delete(tag.id);
 	});
 
 	it("should inserting an tag and returning a id as UUID valid", async () => {
-		const tag = new Tag("Test Tag");
+		const tag = new Tag({ name: "Test Tag" });
 		const result = await tagRepository.insert({ name: tag.name, id: tag.id });
 		expect(validate(result)).toEqual(true);
 		await tagRepository.delete(tag.id);
 	});
 
 	it("should return a tag according to the id passed", async () => {
-		const tag = new Tag("Test Tag");
+		const tag = new Tag({ name: "Test Tag" });
 		await tagRepository.insert({ name: tag.name, id: tag.id });
 		const result = await tagRepository.selectById(tag.id);
 		expect(result.id).toEqual(tag.id);
 		await tagRepository.delete(tag.id);
 	});
 
-	it("should return 13 tags", async () => {
-		const result = await tagRepository.selectAll();
-		expect(result.length).toEqual(13);
+	it("should return 3 tags", async () => {
+		const result = await tagRepository.selectAll(1, 3);
+		expect(result.length).toEqual(3);
 	});
 
 	it("should update successfully", async () => {
-		const tag = new Tag("Test Tag");
+		const tag = new Tag({ name: "Test Tag" });
 		await tagRepository.insert({ name: tag.name, id: tag.id });
 		const result = await tagRepository.update(
 			{ name: "Test Tag updated" },
@@ -43,10 +43,10 @@ describe("Tag - Repository", () => {
 	});
 
 	it("must create a new tag instance with valid values ​​from the repository", async () => {
-		const tag = new Tag("Test Tag");
+		const tag = new Tag({ name: "Test Tag" });
 		await tagRepository.insert({ name: tag.name, id: tag.id });
 		const result = await tagRepository.selectById(tag.id);
-		const tagSearched = new Tag(result.name, result.id);
+		const tagSearched = new Tag({ name: result.name, id: result.id });
 		expect(tagSearched.id).toEqual(tag.id);
 		expect(tagSearched.name).toEqual(tag.name);
 	});
