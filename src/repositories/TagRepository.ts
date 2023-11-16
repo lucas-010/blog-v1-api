de7dc2ee09393c4079732a4ced44edf3a0c556cf
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-11-16 00:27:17 -0300

    tag repository

---
+++ src/repositories/TagRepository.ts
@@ -0,0 +1,19 @@
+import { Tag, TagProps } from "../entities/Tag";
+import { RepositoryBase } from "./RepositoryBase";
+
+export class TagRepository extends RepositoryBase<Tag> {
+	constructor() {
+		super("tag");
+	}
+
+	async insert(item: TagProps): Promise<string> {
+		return (await this.database(this.tableName).insert(item).returning("id"))[0]
+			.id;
+	}
+
+	async update(item: Omit<TagProps, "id">, id: string): Promise<boolean> {
+		return (
+			(await this.database(this.tableName).where("id", id).update(item)) > 0
+		);
+	}
+}
