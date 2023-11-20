381d2f18c35ae1381c5793bc3fc6762ca3fb6c4a
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-11-20 16:50:01 -0300

    comment schema optional created

--- src/schemas/commentSchema.ts
+++ src/schemas/commentSchema.ts
@@ -7,3 +7,10 @@ export const commentSchema = z.object({
 	articleId: z.string().uuid({ message: "Deve ser um ID único universal" }),
 	userId: z.string().uuid({ message: "Deve ser um ID único universal" }),
 });
+
+export const commentSchemaOptional = z.object({
+	text: z
+		.string()
+		.min(1, { message: "Deve ter no mínimo 1 caractere" })
+		.optional(),
+});
