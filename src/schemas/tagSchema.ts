f1d16f93720775913340cab396f66510fbbdbd83
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-11-20 16:50:41 -0300

    tag schema optional created

--- src/schemas/tagSchema.ts
+++ src/schemas/tagSchema.ts
@@ -7,3 +7,11 @@ export const tagSchema = z.object({
 		.min(2, "Deve ter no mínimo 2 caracteres")
 		.max(20, "Deve ter no máximo 20 caracteres"),
 });
+
+export const tagSchemaOptional = z.object({
+	name: z
+		.string()
+		.min(2, "Deve ter no mínimo 2 caracteres")
+		.max(20, "Deve ter no máximo 20 caracteres")
+		.optional(),
+});
